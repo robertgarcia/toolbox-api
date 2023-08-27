58c27a04b43283e1581c2c99e3113488d73c8ad1
Robert D. Garcia <robertdavid_mgt@hotmail.es>
2023-08-27 02:55:23 -0600

    add validation for empty file name

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -1,13 +1,13 @@
 const formatFileCSV = (data) => {
+    
     const lines = data.split('\n');
     const headers = lines[0].split(',');
-    const file = lines[1].split(',')[0];
-  
+    const file =  lines.length > 1 ? lines[1].split(',')[0] : '';
     const jsonData = {
         file,
-        lines: []
+        lines: [],
+        success: lines.length > 1 ? true : false
     };
-  
     for (let i = 1; i < lines.length; i++) {
         const values = lines[i].split(',');
         if (values.length === headers.length) {

3d47a926442561b6f7686614b5653483bc2e0734
Robert D. Garcia <robertdavid_mgt@hotmail.es>
2023-08-27 02:34:08 -0600

    change function format CSV to JSON

---
+++ src/utils/formatter.js
@@ -0,0 +1,26 @@
+const formatFileCSV = (data) => {
+    const lines = data.split('\n');
+    const headers = lines[0].split(',');
+    const file = lines[1].split(',')[0];
+  
+    const jsonData = {
+        file,
+        lines: []
+    };
+  
+    for (let i = 1; i < lines.length; i++) {
+        const values = lines[i].split(',');
+        if (values.length === headers.length) {
+            const lineData = {
+                text: values[1],
+                number: parseInt(values[2]),
+                hex: values[3]
+            };
+            jsonData.lines.push(lineData);
+        }
+    }
+  
+    return jsonData;
+};
+
+export default formatFileCSV;
